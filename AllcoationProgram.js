5f1a111255834d233b1d788b09cadd43ac239317
kaandersen <karenmoeller17@gmial.com>
2022-03-24 10:07:27 +0100

    main File of the program. Up 4 disussion.

---
+++ AllcoationProgram.js
@@ -0,0 +1,13 @@
+/* ***************************************************************************************************************
+* This program is created by group a319 from Aalborg University.... [add description]
+* 
+* This is our main file 
+**************************************************************************************************************** */
+import fs from 'fs';  //enable use of file system
+export {};
+
+
+
+
+
+
