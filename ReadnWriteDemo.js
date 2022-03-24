7afc7375e65ef4cbd4a89b42e40da0cdd4d5081a
kaandersen <karenmoeller17@gmial.com>
2022-03-24 11:15:33 +0100

    example of reading and writing txt

---
+++ ReadnWriteDemo.js
@@ -0,0 +1,28 @@
+/* ***************************************************************************************************************
+*   Example of reading and writing local .txt files
+* 
+**************************************************************************************************************** */
+
+// the fs.readFile() method
+// Include fs module
+import { readFile, writeFile } from 'fs';
+
+/* Test Reading a text file */
+readFile('DemoRead.txt', 'utf8', function(err, data){
+      
+    // Display the file content
+    console.log(data);
+});
+
+console.log('readFile called'); //Affirmation
+
+
+/* Test writing a text file */
+const content = "ayaya";
+
+writeFile('DemoWrite.txt', content, err => {
+    if(err){
+        console.err;
+        return;
+    }    
+});
