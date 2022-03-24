800df77871be0a0af7b0e00cc4a971a718619026
kaandersen <karenmoeller17@gmial.com>
2022-03-24 15:02:38 +0100

    change in description

--- QuestionnaireSubmission.js
+++ QuestionnaireSubmission.js
@@ -3,11 +3,11 @@
 /*
 *   This Code initializes when the students has filled out the questionnaire and presses the "SUBMIT" button
 *   
-*   - Attatch the student's ID to the answer's they have submitted
+*   - Find / note down the student's ID and save it together with the student's answers and Questions
 *
-*   - A function for checking whether all of the Questionnaire Qs have been filled out, must be dynamic
+*   - (A function for checking whether all of the Questionnaire Qs have been filled out, must be dynamic)
 *
-*   - Ability to store all of the student's answers relative to the questions asked into an object(struct)
+*   - Ability to store all of the student's answers relative to the questions asked into an object(struct) (along with their ID)
 * 
 *   - Create the possibility of accessing the data that was filled out, so that the student can edit their answers
 */
