63fabb245677d9bb8a49dd680407068e456fc04b
Hugin <huginj@gmail.com>
2022-04-01 13:38:40 +0200

    Server is now in the open

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+app.use("/static", express.static(path.resolve(__dirname, "HuginsStuff", "frontend", "static")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "HuginsStuff","frontend", "index.html"));
 });
 
 app.listen(process.env.PORT || 5000, () => console.log("Server running at http://localhost:5000/"));
