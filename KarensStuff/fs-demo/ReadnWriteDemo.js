377d73fff40c79a89b602af61ef8ce69d31fb4cb
Karen A <48069766+kaandersen@users.noreply.github.com>
2022-04-04 12:20:53 +0200

    removed unnecessary item

--- KarensStuff/fs-demo/ReadnWriteDemo.js
+++ KarensStuff/fs-demo/ReadnWriteDemo.js
@@ -4,9 +4,9 @@
 **************************************************************************************************************** */
 
 /* Test writing a text file */
-//import { readFile, writeFile } from 'fs';
 const path = require("path");
 const fs = require("fs");
+
 /* Test Reading a text file */
 data = fs.readFileSync(path.join(__dirname, 'DemoRead.txt'), 'utf-8');
 console.log(data);
