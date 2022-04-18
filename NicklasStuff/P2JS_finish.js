744af7c2c6d0980967675610d731d530d9f63ea8
Karen A <48069766+kaandersen@users.noreply.github.com>
2022-04-18 17:58:05 +0200

    Updated "Finish" page which is more like status

--- NicklasStuff/P2JS_finish.js
+++ NicklasStuff/P2JS_finish.js
@@ -28,3 +28,27 @@ function store(identifier) {
         localStorage.doneFlag = JSON.stringify(done);
     }
 }
+/**
+ * This function related to the search bar 
+ */
+function searchQuestionnaire() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchbar");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("quest-table");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
